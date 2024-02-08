19eca8292e04160a0efdbe79f58e885b24d586ac
abenezertesfaye <abenezertesfaye92@gmail.com>
2024-02-07 16:28:58 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import LandingPage from "./Landing";
+
+function App() {
+  return (
+	<React.Fragment class='bg-[#eee]'>
+		<LandingPage />
+	</React.Fragment>
+  );
+}
+
+export default App;
+
+
